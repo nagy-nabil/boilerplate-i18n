6d27169bd9404315907ffa1496d2ceb77eaf53f4
nagy nabil <nagynabil651@gmail.com>
2024-03-22 06:58:32 +0200

    add new lang example: german example

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,6 +1,6 @@
 import { createSharedPathnamesNavigation } from "next-intl/navigation";
 
-export const locales = ["es", "en"] as const;
+export const locales = ["es", "en", "de"] as const;
 export const localePrefix = "as-needed";
 
 export const { Link, redirect, usePathname, useRouter } =
