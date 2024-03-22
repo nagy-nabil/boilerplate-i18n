774ada57b79b9f0836fb66f7b12cc5809685cafc
nagy nabil <nagynabil651@gmail.com>
2024-03-22 06:54:15 +0200

    fix: i18n not working with default lang in nested routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,14 @@
-import createMiddleware from 'next-intl/middleware';
- 
+import createMiddleware from "next-intl/middleware";
+
 export default createMiddleware({
-  // A list of all locales that are supported
-  locales: ['es', 'en'],
-  // Used when no locale matches
-  defaultLocale: 'en',
-  localePrefix: 'as-needed'
+    // A list of all locales that are supported
+    locales: ["es", "en"],
+    // Used when no locale matches
+    defaultLocale: "en",
+    localePrefix: "as-needed",
 });
- 
+
 export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(en|es)/:path*']
+    // Match only internationalized pathnames
+    matcher: ["/", "/((?!api|_next|_vercel|.*\\..*).*)", "/([\\w-]+)?//(.+)"],
 };
