5a5345228fdeaae48a859ae19ac7b42cd76ba683
nagy nabil <nagynabil651@gmail.com>
2024-03-23 05:46:47 +0200

    chore: move default locale to navigation.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,11 @@
 import createMiddleware from "next-intl/middleware";
-import { locales, localePrefix } from "./navigation";
+import { locales, localePrefix, defaultLocale } from "./navigation";
 
 export default createMiddleware({
   // A list of all locales that are supported
   locales,
   // Used when no locale matches
-  defaultLocale: "en",
+  defaultLocale,
   localePrefix,
 });
 
