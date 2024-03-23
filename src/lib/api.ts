350660bd9bda653ce52d2397432950bf7f31f089
nagy nabil <nagynabil651@gmail.com>
2024-03-23 05:50:04 +0200

    chore: force the api functions to get locale

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,28 +1,41 @@
 import { Post } from "@/interfaces/post";
+import { defaultLocale } from "@/navigation";
 import fs from "fs";
 import matter from "gray-matter";
 import { join } from "path";
 
 const postsDirectory = join(process.cwd(), "_posts");
+const defaultPostsDir = join(postsDirectory, defaultLocale)
 
+/**
+ * return slugs based on default locale `en`
+ *
+ * @Note will return the slug.md
+ */
 export function getPostSlugs() {
-  return fs.readdirSync(postsDirectory);
+  return fs.readdirSync(defaultPostsDir);
 }
 
-export function getPostBySlug(slug: string, locale?: string) {
-  console.log("DEBUGPRINT[1]: api.ts:12: slug, locale=", {slug, locale})
+/**
+ * @param slug string expected to contains the extension `slug`.md
+ * @default locale en
+  */
+export function getPostBySlug(slug: string, locale: string) {
   const realSlug = slug.replace(/\.md$/, "");
-  const fullPath = locale ? join(postsDirectory, `${realSlug}-${locale}.md`) : join(postsDirectory, `${realSlug}.md`);
+  const fullPath =  join(postsDirectory, locale, `${realSlug}.md`);
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data, content } = matter(fileContents);
 
   return { ...data, slug: realSlug, content } as Post;
 }
 
-export function getAllPosts(): Post[] {
+/**
+ * @default locale en
+*/
+export function getAllPosts(locale: string): Post[] {
   const slugs = getPostSlugs();
   const posts = slugs
-    .map((slug) => getPostBySlug(slug))
+    .map((slug) => getPostBySlug(slug, locale))
     // sort posts by date in descending order
     .sort((post1, post2) => (post1.date > post2.date ? -1 : 1));
   return posts;
