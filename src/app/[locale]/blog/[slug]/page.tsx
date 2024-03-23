b58c304e3d2f8f56f4dceda7c129a4b64c76eb6a
nagy nabil <nagynabil651@gmail.com>
2024-03-23 03:48:02 +0200

    feat: render md

---
+++ src/app/[locale]/blog/[slug]/page.tsx
@@ -0,0 +1,72 @@
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { getAllPosts, getPostBySlug } from "@/lib/api";
+import { CMS_NAME } from "@/lib/constants";
+import markdownToHtml from "@/lib/markdownToHtml";
+import Container from "@/app/_components/container";
+import { PostBody } from "@/app/_components/post-body";
+import { PostHeader } from "@/app/_components/post-header";
+import Header from "@/app/_components/header";
+
+type Params = {
+	params: {
+		slug: string;
+		locale: string
+	};
+};
+
+export default async function Post({ params }: Params) {
+  console.log("DEBUGPRINT[3]: page.tsx:18: params=", params)
+  const post = getPostBySlug(params.slug);
+
+  if (!post) {
+    return notFound();
+  }
+
+  const content = await markdownToHtml(post.content || "");
+
+  return (
+    <main>
+      <Container>
+        <Header />
+        <article className="mb-32">
+          <PostHeader
+            title={post.title}
+            coverImage={post.coverImage}
+            date={post.date}
+            author={post.author}
+          />
+          <PostBody content={content} />
+        </article>
+      </Container>
+    </main>
+  );
+}
+
+
+export function generateMetadata({ params }: Params): Metadata {
+  // TODO: pass the locale
+  const post = getPostBySlug(params.slug);
+
+  if (!post) {
+    return notFound();
+  }
+
+  const title = `${post.title} | Next.js Blog Example with ${CMS_NAME}`;
+
+  return {
+    title,
+    openGraph: {
+      title,
+      images: [post.ogImage.url],
+    },
+  };
+}
+
+export async function generateStaticParams() {
+  const posts = getAllPosts();
+
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
